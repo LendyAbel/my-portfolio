0531f3439a9a04a4f074be92528a3c62cfd2bc3d
LendyAbel <lendyabel93@gmail.com>
2025-07-15 09:24:19 +0200

    prueba 2

--- vite.config.ts
+++ vite.config.ts
@@ -1,16 +1,15 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 import tailwindcss from '@tailwindcss/vite'
-import path from "path"
+import tsconfigPaths from 'vite-tsconfig-paths' // ↓ npm i -D vite-tsconfig-paths
 
 export default defineConfig({
   plugins: [
-    react(),
     tailwindcss(),
+    react(),
+    tsconfigPaths(),
   ],
   resolve: {
-    alias: {
-      "@": path.resolve(__dirname, "./src"),
-    },
+    alias: { '@': '/src' },
   },
 })
